49218410787948669cef43802c1b2792fb96b1b6
tyler <tylerwilkerson75@gmail.com>
2024-11-11 12:09:29 -0500

    deleted fingertables

--- chord.py
+++ chord.py
@@ -1,146 +1,151 @@
-from pprint import pprint
+from threading import Thread
+import time
+from typing import List, Dict, Optional
+from Node import Node
+import random
+from enum import Enum
 
-def binarySearch(alist, item):
-    """
-    binary search given alist and item to search for
-    returns is element was found, otherwise return the last midpoint before search failed
-    last midpoint is always either the predessecor or succesor to item (if the item is not found)
-    """
-    first = 0
-    last = len(alist)-1
-    found = False
-    
-    while first<=last and not found:
-        midpoint = (first + last)//2
-        if alist[midpoint] == item:
-            found = True
-        else:
-            if item < alist[midpoint]:
-                last = midpoint-1
-            else:
-                first = midpoint+1
-    
-    return found, midpoint
+class ChurnRateTimer(Enum):
+    LOW = 20
+    MEDIUM = 50
+    HIGH = 100
 
-def successor(L, n):
-    """
-    Given a sorted list of ints L, and an integer n, return the smallest element of L that
-    is larger than or equal to n. If none exist, return None
-    """
-    found, midpoint = binarySearch(L,n)
-    if L[midpoint] >= n:
-        return L[midpoint]
-    if L[midpoint] < n:
-        if midpoint==len(L)-1:
-            return None
-        return L[midpoint+1]
-
-
-def get_fingers(nodes, m):
-    """
-    build finger table, given a list of nodes, and m
-    """
-    nodes_augment = nodes.copy()
-    nodes_augment = sorted(nodes_augment)
-    maxnum = pow(2,m)
-    # wraparound first node to accomodate nodes clockwise of n when n is larger than the largest valued node
-    nodes_augment.append(nodes_augment[0]+maxnum)
-    
-    finger_table = {}
-    for node in nodes: 
-        fingers = [None]*m
-        for i in range(m):
-            j = (node + pow(2,i))%maxnum
-            fingers[i] = successor(nodes_augment,j)%maxnum
-        finger_table[node] = fingers.copy()
+class ChordNetwork:
+    def __init__(self, size, r, bank_size):
+        self.size = size
+        self.r = r # number of successor nodes a node can have
+        self.node_bank: Dict[int, Node] = self.initialize_node_bank(bank_size)
+        self.graph: List[Node] = self.initialize_graph(size, r)
         
-    return finger_table
+        self.timer_thread = Thread(target=self.update_timers)
+        self.timer_thread.start()
+    
+    def initialize_node_bank(self, bank_size):
+        print('Initializing node bank...')
+        bank = {}
+        for i in range(bank_size):
+            if i not in bank:
+                timer = self.random_churn_rate()
+                node = Node(id=i, ttl=timer, active_status=False)
+                bank[node.id] = node
+        return bank
 
-def get_key_loc(nodes, k, m):
-    """
-    given a key location k, and ring of size 2^m, and list of nodes, give the location of the key in the DHT
-    """
-    nodes_augment = nodes.copy()
-    nodes_augment = sorted(nodes_augment)
-    maxnum = pow(2,m)
-    # wraparound first node to accomodate nodes clockwise of n when n is larger than the largest valued node
-    nodes_augment.append(nodes_augment[0]+maxnum)
-    return successor(nodes_augment, k)%maxnum
+    def initialize_graph(self, size, r):
+        print('Initializing graph...')
+        graph = []
+        for i in range(size):
+            node = self.node_bank[i]
+            node.set_active_status(True)
+            self.assign_initial_successors_and_predecessors(node, size, r)
+            graph.append(node)
+        return graph 
+      
+    def assign_initial_successors_and_predecessors(self, node: Node, size, r):
+        # Successor assignment
+        for j in range(1, r+1):
+            successor_id = (node.id + j) % size
+            node.finger_table['successors'].append(successor_id)
+
+        # Predecessor assignment
+        predecessor_id = (node.id - 1) % size
+        node.finger_table['predecessors'].append(predecessor_id)
+
+    def update_timers(self):
+        while True:
+            for node in self.node_bank.values():
+                node.ttl -= 1
+                if node.ttl <= 0:
+                    if node.is_active:
+                        print(f"Node {node.id} is attempting to leave the network...")
+                        self.leave_network(node)
+                    else:
+                        print(f"Node {node.id} is attempting to join the network...")
+                        self.join_network(node)
+            time.sleep(1)
+
+    def join_network(self, node:Node):
+        if node in self.graph:
+            print(f"Node {node.id} is already in the network. Cannot add to network")
+            return
+        
+        node.set_active_status(True)
+        node.reset_timer()
+        self.add_node(node)
+
+        self.update_all_finger_tables()
+        print(f"Node {node.id} has joined the network. \n")
+
+    def leave_network(self, node: Node):
+        if node not in self.graph:
+            print(f"\t Node {node.id} is not in the network. Cannot remove from network")
+            return
+        
+        # Remove node and reassign successors/predecessors
+        node.set_active_status(False)
+        node.reset_timer()
+        self.remove_node(node)
+        
+        self.update_all_finger_tables()
+        print(f"Node {node.id} has left the network. \n")
+
+    def update_all_finger_tables(self):
+        print('updating finger tables ...')
+        for idx, node in enumerate(self.graph):
+            # update predecessor
+            predecessor_node = self.graph[idx - 1] # we can use negative numbers to wrap around an array in python
+            node.finger_table['predecessors']= [predecessor_node.id]
+
+            # update successors
+            # print('idx is:', idx)
+            new_successors_list = []
+            for i in range(idx + 1, (idx + self.r + 1)):
+                modulo_idx = i % len(self.graph) # modulo index to ensure wrap around
+                # print(f'\t i is :{i},  modulo_idx is { modulo_idx}')
+                new_successors_list.append(self.graph[modulo_idx].id)
+            node.finger_table['successors']= new_successors_list
+
+        # Display network to check that all tables were updated accordingly
+        graph_list = []
+        for node in self.graph:
+            graph_list.append(str(node.id))
+        print('After updating finger table, graph list is:', '->'.join(graph_list))
+        # self.display_network()
+
+    def display_network(self):
+        print(" Network state:")
+        for node in self.graph:
+            print(node)
+
+    def add_node(self, new_node):
+        # find position to add node
+        # Implemented with linear search. Might want to change to binary in future
+        idx = 0 
+        while idx < len(self.graph) and self.graph[idx].id < new_node.id:
+            idx+= 1 
+
+        self.graph.insert(idx, new_node)
+
+    def remove_node(self, node):
+        self.graph = [n for n in self.graph if n.id != node.id]
+
+    def random_churn_rate(self):
+        rand = random.random()
+        if rand < 0.333:
+            return ChurnRateTimer.LOW.value
+        elif rand < 0.666:
+            return ChurnRateTimer.MEDIUM.value
+        return ChurnRateTimer.HIGH.value
+        
 
 
-def get_query_path(nodes, k, n, m):
-    """
-    compute the quey path, given a list of nodes in the DHT, the key k to be found, origin node n, and ring of size 2^m
-    """
-    nodes_copy = nodes.copy()
-    nodes_copy = sorted(nodes_copy)
-    
-    k_loc = get_key_loc(nodes_copy, k, m) # location of key k
-    fingers = get_fingers(nodes_copy, m) # finger table
-    query_path = [n] # start the path with origin node
-    
-    while(True): #keep going until the correct key location is found
-        
-        if k_loc==n: # return the path if correct location found
-            return query_path
 
-        f = fingers[n]
-        
-        i = 0
-        x = f[i]
-        
-        if k_loc > n: 
-            # if the clockwise path between the origin node (n) and the key location (k_loc) DOES NOT include 0, all the nodes that
-            # lie in that path (x) must satisfy k_loc> x >n
-            while (x < k_loc) and (x > n):
-                i += 1
-                if i == len(f):
-                    break
-                x = f[i]
-                
-        if k_loc < n: 
-            # if the clockwise path between the origin node and the key location includes 0, all the nodes that
-            # lie in that path (x) must satisfy (k_loc < x) && (n < x) OR (k_loc > x) && (n > x)
-            while ( ((x > k_loc) and (x > n)) or ((x < k_loc) and (x < n)) ):
-                i += 1
-                if i == len(f):
-                    break
-                x = f[i]
-        
-        # update next node to query, and append to path
-        if i==0:
-            n = f[0]
-        else:
-            n = f[i-1]
-            
-        query_path.append(n)
+def test_chord_protocol():
+    # Create a Chord network with 5 nodes and 2 successors for each node
+    # Node bank size of 10 nodes
+    network = ChordNetwork(size=5, r=2, bank_size=10)
     
-if __name__=="__main__":
-    
-    # Chord DHT as described in Lecture 5 of week 3 of the Coursera Cloud Computing Concepts Part 1 Course
-    
-    nodes = [112, 96, 80, 16, 32, 45] # nodes in the P2P system
-    m = 7 # Ring is of size 2^m
-    print('Chord DHT with peers {nodes}, and m = {m} \n'.format(nodes=nodes, m=7))
-    
-    print('The finger table for this DHT is: ')
-    pprint(get_fingers(nodes, m))
-    print()
-    
-    file_key = 42
-    print('File location for file key {file_key} is at node {file_key_location} \n'.format(
-        file_key=file_key, file_key_location=get_key_loc(nodes, file_key, m)
-    ))
-    query_node = 80
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
-    query_node = 96
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
-    query_node = 45
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
-    
+    # Display initial state of the network
+    print("Initial Network State:")
+    network.display_network()
+
+test_chord_protocol()
