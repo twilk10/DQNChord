abc45615899a1dd02f7e5c8b73feae53804a34dc
Boluwarin Dairo <dairobolu@gmail.com>
2024-11-17 17:59:44 -0500

    created a new version of chord

--- Chord.py
+++ Chord.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Lock
 import time
 from typing import List, Dict, Optional
 from Node import Node
@@ -11,14 +11,17 @@ class ChurnRateTimer(Enum):
     HIGH = 100
 
 class ChordNetwork:
-    def __init__(self, size, r, bank_size):
-        self.size = size
+    def __init__(self,size, r, bank_size, start_timer_thread = True):
         self.r = r # number of successor nodes a node can have
         self.node_bank: Dict[int, Node] = self.initialize_node_bank(bank_size)
-        self.graph: List[Node] = self.initialize_graph(size, r)
+        self.initialize_graph(size, r)
+
+        self.lock = Lock()
+        self.timer_thread = None
+        if start_timer_thread:
+            self.timer_thread = Thread(target=self.update_node_timers)
+            self.timer_thread.start()
         
-        self.timer_thread = Thread(target=self.update_timers)
-        self.timer_thread.start()
     
     def initialize_node_bank(self, bank_size):
         print('Initializing node bank...')
@@ -26,31 +29,55 @@ class ChordNetwork:
         for i in range(bank_size):
             if i not in bank:
                 timer = self.random_churn_rate()
-                node = Node(id=i, ttl=timer, active_status=False)
+                status = False
+
+                # set timer to a large number for Agent Node 0 and status to True
+                if i == 0:
+                    timer = float('inf')
+                    status = True
+
+                node = Node(id=i, ttl=timer, active_status=status)
                 bank[node.id] = node
         return bank
-
+    
     def initialize_graph(self, size, r):
-        print('Initializing graph...')
-        graph = []
-        for i in range(size):
+        # Activate the first 'size' nodes from the node bank
+        for i in range(1, size):
             node = self.node_bank[i]
-            node.set_active_status(True)
-            self.assign_initial_successors_and_predecessors(node, size, r)
-            graph.append(node)
-        return graph 
-      
-    def assign_initial_successors_and_predecessors(self, node: Node, size, r):
-        # Successor assignment
-        for j in range(1, r+1):
-            successor_id = (node.id + j) % size
-            node.finger_table['successors'].append(successor_id)
+            node.is_active = True
+            self.assign_successors_and_predecessors(node, r)
 
-        # Predecessor assignment
-        predecessor_id = (node.id - 1) % size
-        node.finger_table['predecessors'].append(predecessor_id)
+        # Ensure the special node (Node 0) is set up
+        self.assign_successors_and_predecessors(self.node_bank[0], r)
 
-    def update_timers(self):
+    def assign_successors_and_predecessors(self, node: Node, r: int):
+        # Get the list of active node IDs in ascending order
+        active_nodes_ids = sorted([n.id for n in self.node_bank.values() if n.is_active])
+        total_number_of_active_nodes = len(active_nodes_ids)
+
+        # Validate input
+        if node.id not in active_nodes_ids:
+            raise ValueError(f"Node {node.id} is not active or missing from the active nodes.")
+        if r > total_number_of_active_nodes:
+            raise ValueError(f"Not enough active nodes to assign {r} successors/predecessors.")
+        
+        # Get the index of the current node in the sorted active nodes list
+        node_index = active_nodes_ids.index(node.id)
+        
+        # Assign successors (based on Chord's finger table definition)
+        node.finger_table['successors'] = []
+        for i in range(r):
+            successor_index = (node_index + pow(2, i)) % total_number_of_active_nodes
+            successor_node_id = active_nodes_ids[successor_index]
+            node.finger_table['successors'].append(successor_node_id)
+        
+        # Assign predecessor (only one predecessor for this use case)
+        predecessor_index = (node_index - 1) % total_number_of_active_nodes
+        predecessor_node_id = active_nodes_ids[predecessor_index]
+        node.finger_table['predecessors'] = [predecessor_node_id]
+
+
+    def update_node_timers(self):
         while True:
             for node in self.node_bank.values():
                 node.ttl -= 1
@@ -61,71 +88,47 @@ class ChordNetwork:
                     else:
                         print(f"Node {node.id} is attempting to join the network...")
                         self.join_network(node)
+
+            # Have Node 0 act
+            # agent_node = self.node_bank[0]
+            # agent_node.act(self)
             time.sleep(1)
 
-    def join_network(self, node:Node):
-        if node in self.graph:
-            print(f"Node {node.id} is already in the network. Cannot add to network")
-            return
-        
-        node.set_active_status(True)
-        node.reset_timer()
-        self.add_node(node)
+    def join_network(self, node: Node):
+        with self.lock:
+            node.set_active_status(True)
+            node.reset_timer()
+            self.assign_successors_and_predecessors(node, self.r)
 
-        self.update_all_finger_tables()
-        print(f"Node {node.id} has joined the network. \n")
+            self.update_all_finger_tables()
+            print(f"Node {node.id} has joined the network.\n")
 
-    def leave_network(self, node: Node):
-        if node not in self.graph:
-            print(f"\t Node {node.id} is not in the network. Cannot remove from network")
-            return
-        
-        # Remove node and reassign successors/predecessors
-        node.set_active_status(False)
-        node.reset_timer()
-        self.remove_node(node)
-        
-        self.update_all_finger_tables()
-        print(f"Node {node.id} has left the network. \n")
+    def leave_network(self, node: Node): 
+        with self.lock:
+            
+            node.set_active_status(False)
+            node.reset_timer()
+
+            # Clear the node's finger table
+            node.finger_table = {'predecessors': [], 'successors': []}
+            
+            self.update_all_finger_tables()
+            print(f"Node {node.id} has left the network.\n")
 
     def update_all_finger_tables(self):
-        print('updating finger tables ...')
-        for idx, node in enumerate(self.graph):
-            # update predecessor
-            predecessor_node = self.graph[idx - 1] # we can use negative numbers to wrap around an array in python
-            node.finger_table['predecessors']= [predecessor_node.id]
+        # updates finger tables of active nodes only
+        active_nodes = [node for node in self.node_bank.values() if node.is_active]
+        for node in active_nodes:
+            self.assign_successors_and_predecessors(node, self.r)
 
-            # update successors
-            # print('idx is:', idx)
-            new_successors_list = []
-            for i in range(idx + 1, (idx + self.r + 1)):
-                modulo_idx = i % len(self.graph) # modulo index to ensure wrap around
-                # print(f'\t i is :{i},  modulo_idx is { modulo_idx}')
-                new_successors_list.append(self.graph[modulo_idx].id)
-            node.finger_table['successors']= new_successors_list
-
-        # Display network to check that all tables were updated accordingly
-        graph_list = []
-        for node in self.graph:
-            graph_list.append(str(node.id))
-        print('After updating finger table, graph list is:', '->'.join(graph_list))
-        # self.display_network()
 
     def display_network(self):
         print(" Network state:")
-        for node in self.graph:
-            print(node)
+        for node in self.node_bank.values():
+            if node.is_active:
+                print(node)
 
-    def add_node(self, new_node):
-        # find position to add node
-        # Implemented with linear search. Might want to change to binary in future
-        idx = 0 
-        while idx < len(self.graph) and self.graph[idx].id < new_node.id:
-            idx+= 1 
-
-        self.graph.insert(idx, new_node)
-
-    def remove_node(self, node):
+    def remove_node(self, node: Node):
         self.graph = [n for n in self.graph if n.id != node.id]
 
     def random_churn_rate(self):
@@ -135,17 +138,3 @@ class ChordNetwork:
         elif rand < 0.666:
             return ChurnRateTimer.MEDIUM.value
         return ChurnRateTimer.HIGH.value
-        
-
-
-
-def test_chord_protocol():
-    # Create a Chord network with 5 nodes and 2 successors for each node
-    # Node bank size of 10 nodes
-    network = ChordNetwork(size=5, r=2, bank_size=10)
-    
-    # Display initial state of the network
-    print("Initial Network State:")
-    network.display_network()
-
-test_chord_protocol()
